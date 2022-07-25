e6720edeba1dac79a13d13645e1027a30cba12e9
Brayan Feliz Rodríguez <108096690+Pro-gramador@users.noreply.github.com>
2022-07-25 13:58:57 -0400

    Project1
    1ra práctica de Open Bootcamp

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+        addition(10, 20, 30);
+        int result = 0;
+        var value= addition(10, 20, 30);
+
+        System.out.println(value);
+
+        //Hola a todos jaja, mi primer comentario.
+
+    }
+    public static int addition(int a, int b, int c){
+
+        return a + b + c;
+    }
+
+
+}
